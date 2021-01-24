a132749181a7a80aef08853d7ac99d5f73a8a450
FahimFBA <fahimbinamin@gmail.com>
2021-01-25 01:38:42 +0600

    initial

---
+++ Problem 1/solve.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int N,T,i;
+    scanf("%d", &T);
+    for(i=1;i<=T; i++)
+    {
+       scanf("%d", &N);
+       if (N%2==0) printf("even\n");
+       else printf("odd\n");
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# dimikoj-solve
+# Dimik OJ Solution
+
+### It contains all the solutions of mine which I had solved in Dimik Online Judge.
+
+[You may check my profile in Dimik OJ](https://dimikoj.com/users/5231/FBA)
